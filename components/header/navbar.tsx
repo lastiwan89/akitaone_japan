0a68c28152bd360af27aefab983eb6a9a836be8b
lastiwan89 <59128819+lastiwan89@users.noreply.github.com>
2025-12-05 19:08:46 +0800

    aria attribut added to sidebar & navbar

--- components/header/navbar.tsx
+++ components/header/navbar.tsx
@@ -1,39 +1,41 @@
-"use client";
-
+'use client'
 import {
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-} from "../ui/navigation-menu";
-import { NavigationMenu } from "../ui/navigation-menu";
-import Sidebar from "./sidebar";
-import { NAVLINKS } from "@/constant/navlinks";
-import Link from "next/link";
-import Image from "next/image";
-import { cn } from "@/lib/utils";
+} from '../ui/navigation-menu'
+import { NavigationMenu } from '../ui/navigation-menu'
+import Sidebar from './sidebar'
+import { NAVLINKS } from '@/constant/navlinks'
+import Link from 'next/link'
+import Image from 'next/image'
+import { cn } from '@/lib/utils'
 
 const Navbar = () => {
   return (
     <div
+      aria-label="navigation"
       className={cn(
-        "flex items-center justify-between bg-linear-60 from-black to-cyan-500",
+        'flex items-center justify-between bg-linear-60 from-black to-cyan-500 px-8 py-0.5',
       )}
     >
-      <Link href="/">
+      <Link
+        aria-label="home logo link"
+        className="flex items-center justify-center gap-2"
+        href="/"
+      >
         <Image
           src="/logo-akitaone.png"
           width={70}
           height={70}
           alt="akita one japan travel"
         />
+        <h1 className="font-black text-white uppercase">AkitaOne</h1>
       </Link>
       <NavigationMenu className="hidden md:flex">
-        <NavigationMenuList className="flex items-center justify-center gap-8">
+        <NavigationMenuList className="flex items-center justify-center gap-6">
           {NAVLINKS.map((items) => (
-            <NavigationMenuItem
-              className="text-white hover:text-gray-900"
-              key={items.id}
-            >
+            <NavigationMenuItem className="text-white" key={items.id}>
               <NavigationMenuLink href={items.href}>
                 {items.title}
               </NavigationMenuLink>
@@ -43,7 +45,7 @@ const Navbar = () => {
       </NavigationMenu>
       <Sidebar />
     </div>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
--- components/header/sidebar.tsx
+++ components/header/sidebar.tsx
@@ -1,31 +1,43 @@
 'use client'
-import { NAVLINKS } from "@/constant/navlinks"
-import { Button } from "../ui/button"
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../ui/dropdown-menu"
+import { NAVLINKS } from '@/constant/navlinks'
+import { Button } from '../ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '../ui/dropdown-menu'
 import { IoMenuSharp } from 'react-icons/io5'
-import { useRouter } from "next/navigation"
-import { cn } from "@/lib/utils"
+import { useRouter } from 'next/navigation'
+import { cn } from '@/lib/utils'
 
 const Sidebar = () => {
-    const router = useRouter()
-    return (
-        <div className="md:hidden">
-            <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                    <Button size='icon'>
-                        <IoMenuSharp size={32} />
-                    </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent className={cn("w-screen h-screen text-white items-center flex flex-col gap-4 bg-linear-60 from-gray-950 to-cyan-500 font-semibold")}>
-                    {
-                        NAVLINKS.map((items) => (
-                            <DropdownMenuItem key={items.id} onClick={() => router.push(items.href)}>{items.title}</DropdownMenuItem>
-                        ))
-                    }
-                </DropdownMenuContent>
-            </DropdownMenu>
-        </div>
-    )
+  const router = useRouter()
+  return (
+    <div aria-label="toggle navigation" className="md:hidden">
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button size="icon">
+            <IoMenuSharp size={32} />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent
+          className={cn(
+            'flex h-screen w-screen flex-col items-center gap-4 bg-linear-60 from-gray-950 to-cyan-500 font-semibold text-white',
+          )}
+        >
+          {NAVLINKS.map((items) => (
+            <DropdownMenuItem
+              key={items.id}
+              onClick={() => router.push(items.href)}
+            >
+              {items.title}
+            </DropdownMenuItem>
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  )
 }
 
 export default Sidebar
