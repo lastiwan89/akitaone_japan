9933b7d8629fc61d2d18ab74c889c2bd64e8676e
lastiwan89 <59128819+lastiwan89@users.noreply.github.com>
2025-12-07 19:28:50 +0800

    services list added

---
+++ constant/services.ts
@@ -0,0 +1,24 @@
+interface Services {
+  id: number
+  title: string
+  description: string
+}
+
+export const SERVICES: Services[] = [
+  {
+    id: 1,
+    title: 'what we do',
+    description: 'Travel Agents have evolved into travel solution providers.',
+  },
+  {
+    id: 2,
+    title: 'what we achieve',
+    description:
+      'Our reputation for reliability, the reach of our services, and our deep understanding of the Indonesian markets.',
+  },
+  {
+    id: 3,
+    title: 'at the end',
+    description: 'A relationship where we know our customers well.',
+  },
+]
