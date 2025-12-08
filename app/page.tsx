b77b41cde2f1db666cfb456223018115aa3c95c5
lastiwan89 <59128819+lastiwan89@users.noreply.github.com>
2025-12-08 16:58:54 +0800

    homepage update

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
-import Benefit from "@/components/page-section/benefit"
-import Hero from "@/components/page-section/hero"
-import Sponsors from "@/components/page-section/sponsor"
+import Hero from '@/components/page-section/hero'
+import Services from '@/components/page-section/services'
+import Sponsors from '@/components/page-section/sponsor'
 
 const Home = () => {
-    return (
-        <div className="bg-linear-60 from-fuchsia-950 to-cyan-800">
-            {/* Hero section */}
-            <Hero />
-            {/* Sponsors section */}
-            <Sponsors />
-            {/* benefit section */}
-
-        </div>
-    )
+  return (
+    <div className="flex flex-col items-center bg-linear-60 from-black/90 to-sky-800/70 px-8">
+      {/* Hero section */}
+      <Hero />
+      {/* Sponsors section */}
+      <Sponsors />
+      {/* benefit section */}
+      <Services />
+    </div>
+  )
 }
 
 export default Home
