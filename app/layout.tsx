0f9436ec1ca0878c5e312483cf8c610f8ae36693
lastiwan89 <59128819+lastiwan89@users.noreply.github.com>
2025-12-23 17:21:59 +0800

    layout update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,35 @@
-import type { Metadata } from "next";
-import { Merriweather, Oswald } from "next/font/google";
-import "./globals.css";
-import Header from "@/components/header/header";
+import type { Metadata } from 'next'
+import { Merriweather, Lato } from 'next/font/google'
+import './globals.css'
+import Header from '@/components/header/header'
 
-const oswald = Oswald({
-    variable: "--font-sans",
-    subsets: ["latin"],
-});
+const lato = Lato({
+  variable: '--font-sans',
+  subsets: ['latin'],
+  weight: ['400', '700', '900'],
+})
 
 const merriweather = Merriweather({
-    variable: "--font-serif",
-    subsets: ["latin"],
-});
+  variable: '--font-serif',
+  subsets: ['latin'],
+})
 
 export const metadata: Metadata = {
-    title: "Akita One Japan | Your Discovery Partner",
-    description: "Travel With Us, Your Discovery Partner",
-};
+  title: 'Akita One Japan | Your Discovery Partner',
+  description: 'Travel With Us, Your Discovery Partner',
+}
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode
 }>) {
-    return (
-        <html lang="en">
-            <body
-                className={`${oswald.variable} ${merriweather.variable} antialiased`}
-            >
-                <Header />
-                {children}
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body className={`${lato.variable} ${merriweather.variable} antialiased`}>
+        <Header />
+        {children}
+      </body>
+    </html>
+  )
 }
