9281116731725efee03d23900e34e4498e29b2eb
lastiwan89 <59128819+lastiwan89@users.noreply.github.com>
2026-01-31 21:35:12 +0800

    header first

---
+++ components/header.tsx
@@ -0,0 +1,49 @@
+'use client'
+import Link from 'next/link'
+import { Menu, X } from 'lucide-react'
+import { navLink } from '@/constant/navlink'
+import Image from 'next/image'
+import { useState } from 'react'
+export default function Header() {
+  const [isOpen, setIsOpen] = useState(false)
+  return (
+    <>
+      <header className="flex justify-between items-center px-8 py-3 bg-sky-600">
+        <div>
+          <Link href="/">
+            <Image width={80} height={80} src="/logo.png" alt="Logo" />
+          </Link>
+        </div>
+        <div>
+          <nav className="hidden md:flex gap-6">
+            {navLink.map((items) => (
+              <Link
+                className="font-semibold text-md"
+                key={items.title}
+                href={items.src}
+              >
+                {items.title}
+              </Link>
+            ))}
+          </nav>
+          <button onClick={() => setIsOpen((prev) => !prev)}>
+            {isOpen ? <Menu size={24} /> : <X size={24} />}
+          </button>
+        </div>
+      </header>
+      <aside
+        className={`fixed top-16 left-0 h-full md:w-80 w-screen px-4 bg-gray-900 text-white shadow-2xl z-50 transform transition-transform duration-300 ease-in-out flex flex-col ${
+          !isOpen ? 'translate-x-0' : '-translate-x-full'
+        }`}
+      >
+        <nav className="flex flex-col items-center gap-6 py-3">
+          {navLink.map((items) => (
+            <Link className="font-medium" key={items.title} href={items.src}>
+              {items.title}
+            </Link>
+          ))}
+        </nav>
+      </aside>
+    </>
+  )
+}
---
+++ constant/navlink.ts
@@ -0,0 +1,27 @@
+interface Links {
+  title: string
+  src: string
+}
+
+export const navLink: Links[] = [
+  {
+    title: 'Home',
+    src: '/',
+  },
+  {
+    title: 'Location',
+    src: '/location',
+  },
+  {
+    title: 'Work',
+    src: '/work',
+  },
+  {
+    title: 'About',
+    src: '/about',
+  },
+  {
+    title: 'Contact',
+    src: '/contact',
+  },
+]
